7a741f89428bc948477b7221d59272356350d6b4
00 <ppangkko@gmail.com>
2021-07-14 15:23:28 +0900

    7/14 commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -31,4 +31,4 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/craeate.html'
+    template_name = 'accountapp/create.html'
